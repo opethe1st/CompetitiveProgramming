b673aa9bc929da80b0d314223a566aa9bc010858
Opemipo Ogunkola <ogunks@live.com>
2016-12-29 17:45:00 +0100

    Deleted some files that are now in the folders

--- ArmyGame.py
+++
@@ -1,9 +0,0 @@
-#!/bin/python
-# Week of Code 26 - https://www.hackerrank.com/contests/w26/challenges/game-with-cells
-import sys
-
-
-n,m = raw_input().strip().split(' ')
-n,m = [int(n),int(m)]
-
-print (int(m/2.0+0.5)*int(n/2.0+0.5))
