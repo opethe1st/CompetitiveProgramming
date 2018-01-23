7b9f30deeff5d4c84b1580baef3214e34036c605
Opemipo Ogunkola <ogunks@live.com>
2018-01-23 20:19:46 +0000

    Working version

--- Codility/2018/Ferrum/longest_nonegative_sum.py
+++ Codility/2018/Ferrum/longest_nonegative_sum.py
@@ -11,13 +11,14 @@ def solution(A):
     prefix_sum = [0 for i in range(len(A)+1)]
     for i, value in enumerate(A):
         prefix_sum[i+1] = prefix_sum[i] + value
+
     greatest_sum_position_backwards = [(prefix_sum[-1], len(A))]
     for position, value in reversed(list(enumerate(prefix_sum))):
         if greatest_sum_position_backwards[-1][0] < value:
             greatest_sum_position_backwards.append((value, position))
     max_length = 0
     for i, s in enumerate(prefix_sum):
-        position = find_max_non_negative_position(gs=greatest_sum_position_backwards, i=i, s=s)
+        position = find_max_non_negative_position(gs=greatest_sum_position_backwards, s=s)
         if position and max_length < (position - i):
             max_length = (position - i)
     return max_length
@@ -56,12 +57,21 @@ class TestFunction(TestCase):
         ans = solution(A=[-1, -1, 0, 1, 1])
         self.assertEqual(ans, 5)
 
+    def test_9(self):
+        ans = solution(A=[1, 1, 1, 1, 1])
+        self.assertEqual(ans, 5)
+
+    def test_10(self):
+        ans = solution(A=[-1, -1, -1, -1, 1])
+        self.assertEqual(ans, 2)
+
+    def test_11(self):
+        ans = solution(A=[-1, -1, -1, 1, 1])
+        self.assertEqual(ans, 4)
+
+    def test_12(self):
+        ans = solution(A=[-1, 1, -1, -1, 1])
+        self.assertEqual(ans, 4)
+
 if __name__ == '__main__':
-    # assert solution(A=[0, 0, 0]) == 3
-    # print(solution(A=[1, 1, 1]))
-    # assert solution(A=[1, 1, 1]) == 3
-    # assert solution(A=[-1, 1, 1]) == 3
-    # assert solution(A=[1, 1, 1]) == 3
-    assert solution(A=[-1, -1, 1]) == 2
-    # assert solution(A=[-1, -1, -1]) == 0
     main()
