67b62ad38b09724ae356c77b490ce9bfaa89a2cb
Opemipo Ogunkola <ogunks@live.com>
2018-02-10 19:30:33 +0000

    v1 broken clock

--- CodeChef/2018/02/FebruaryChallenge/broken_clock.py
+++ CodeChef/2018/02/FebruaryChallenge/broken_clock.py
@@ -1,5 +1,3 @@
-import sys
-sys.setrecursionlimit = 1000000
 mod = 1000000007
 # mod = 107
 
@@ -17,25 +15,35 @@ def inverse(a):
     else:
         return x % mod
 
+def sqrt(x):
+    return pow(x, ((mod+1)//4), mod)
+# print(mod%4, mod//4)
+n = -1
+print(n, sqrt(n), pow(sqrt(n), 2, mod))
+assert(n == pow(sqrt(n), 2, mod))
 
 def solution(I, D, T):
-    count = 0
     cos_cache = {}
+    sin_cache = {}
 
     cos_cache[1] = (D * inverse(I)) % mod
     def cos(t):
-        nonlocal count
         if t in cos_cache:
             return cos_cache[t]
         else:
-            count += 1
-            c = cos(t//2)
-            cos_cache[t] = (2*pow(c, 2, mod) - 1)%mod if t%2 == 0 else (2*cos(t-1)*cos(1) - cos(t-2))%mod
+            cos_cache[t] = (2*pow(cos(t//2), 2, mod) - 1)%mod if t%2 == 0 else 2*(cos(t-1)*cos(1) - sin(t-1)*sin(1))%mod
             return cos_cache[t]
-    # p = len(bin(T))-1
-    # cos(t=(1<<p)-1)
+    sin_square = (1 - pow(cos_cache[1], 2, mod))
+    sin_cache[1] = sqrt(x=sin_square) # how to know which quadrant?
+    def sin(t):
+        if t in sin_cache:
+            return sin_cache[t]
+        else:
+            sin_cache[t] = (2*sin(t//2)*cos(t//2))%mod if t%2 == 0 else (sin(t-1)*cos(1) + cos(t-1)*sin(1))%mod
+            return sin_cache[t]
     ans = (cos(t=T)*I)%mod
-    print('num of computations', count)
+    # print('hereer sin_cache', sin_cache)
+    # print('herere  cos_cache', cos_cache)
     return ans
 
 
