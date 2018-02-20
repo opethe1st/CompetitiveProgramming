81439999669052497b93f183e4e36f70262ce143
Opemipo Ogunkola <ogunks@live.com>
2018-02-20 22:37:33 +0000

    Codility - Ferrum

--- CodeChef/2018/02/FebruaryChallenge/broken_clock.py
+++ CodeChef/2018/02/FebruaryChallenge/broken_clock.py
@@ -17,34 +17,35 @@ def inverse(a):
 
 def sqrt(x):
     ans = pow(x, ((mod+1)//4), mod)
-    return ans if x**2 else 0
-# print(mod%4, mod//4)
-n = -1
-print(n, sqrt(n), pow(sqrt(n), 2, mod))
-# assert(n == pow(sqrt(n), 2, mod))
+    if pow(ans, 2, mod) != x:
+        return 0
+    return ans
+
 
 def solution(I, D, T):
     cos_cache = {}
     sin_cache = {}
     count = 0
     cos_cache[1] = (D * inverse(I)) % mod
+    sin_square = (1 - pow(cos_cache[1], 2, mod))%mod
+    sin_cache[1] = sqrt(x=sin_square) if sin_square else 1
+    multiplier = 1 - pow(cos_cache[1], 2, mod) if not(sin_square) else 1
     def cos(t):
         nonlocal count
         count+=1
         if t in cos_cache:
             return cos_cache[t]
         else:
-            cos_cache[t] = (2*pow(cos(t//2), 2, mod) - 1)%mod if t%2 == 0 else 2*(cos(t-1)*cos(1) - sin(t-1)*sin(1))%mod
+            cos_cache[t] = (2*pow(cos(t//2), 2, mod) - 1)%mod if t%2 == 0 else (cos(t-t//2)*cos(t//2) - sin(t-t//2)*sin(t//2)*multiplier)%mod
             return cos_cache[t]
-    sin_square = (1 - pow(cos_cache[1], 2, mod))
-    sin_cache[1] = sqrt(x=sin_square) # how to know which quadrant?
+
     def sin(t):
         nonlocal count
         count+=1
         if t in sin_cache:
             return sin_cache[t]
         else:
-            sin_cache[t] = (2*sin(t//2)*cos(t//2))%mod if t%2 == 0 else (sin(t-1)*cos(1) + cos(t-1)*sin(1))%mod
+            sin_cache[t] = (2*sin(t//2)*cos(t//2))%mod if t%2 == 0 else (sin(t-t//2)*cos(t//2) + cos(t-t//2)*sin(t//2))%mod
             return sin_cache[t]
     ans = (cos(t=T)*I)%mod
     # print('', count)
